57cb028bcd8d8c364be5482a3523d5fa427ec207
daraghmartin <d.martin@base2services.com>
2016-06-09 17:11:43 +1000

    add docker and base icinga2

---
+++ recipes/install.rb
@@ -0,0 +1,28 @@
+
+package 'libcgroup' do
+ action :install
+end
+
+service 'cgconfig' do
+  action [:enable, :start]
+end
+
+package "docker"
+
+service 'docker' do
+  action [:enable, :start]
+end
+
+docker_image 'icinga/icinga2'
+
+directory '/usr/src/icinga2'
+
+template '/usr/src/icinga2/Dockerfile' do
+  source 'docker/base2-icinga2.erb'
+end
+
+docker_image 'base2-icinga2' do
+  tag 'v0.1.0'
+  source '/usr/src/icinga2'
+  action :build_if_missing
+end
