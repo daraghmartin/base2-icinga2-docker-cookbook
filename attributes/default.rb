9fb8df4d2f1291bed385100b0af5c14c6f47a17a
daraghmartin <d.martin@base2services.com>
2016-06-28 15:35:05 +1000

    add more stuff

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,4 @@
 default["base2"]["icinga2"]["pagerduty"]["api_key"] = "x"
 default["base2"]["icinga2"]["container"]["tag"] = 'v0.1.0'
-default["base2"]["icinga2"]["container"]["bind_dirs"] = ['/data/icinga2/mysql/', '/data/icinga2/conf.d/extra']
-default["base2"]["icinga2"]["container"]["binds"] = ['/data/icinga2/conf.d/extra:/etc/icinga2/conf.d/extra', '/data/icinga2/mysql:/var/lib/mysql']
+default["base2"]["icinga2"]["container"]["bind_dirs"] = ['/data/icinga2/mysql/', '/data/icinga2/conf.d', '/data/opt/base2/icinga2/conf.d']
+default["base2"]["icinga2"]["container"]["binds"] = ['/data/icinga2/conf.d:/etc/icinga2/conf.d', '/data/opt/base2/icinga2/conf.d:/opt/base2/icinga2/conf.d' '/data/icinga2/mysql:/var/lib/mysql']
