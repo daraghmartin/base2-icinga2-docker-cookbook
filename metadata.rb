fbcae937a6843b07fe22633d8d248c167ca446c5
daraghmartin <d.martin@base2services.com>
2016-08-04 04:51:20 +1000

    bump

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'you@example.com'
 license 'all_rights'
 description 'Installs/Configures base2-icinga2-docker'
 long_description 'Installs/Configures base2-icinga2-docker'
-version '0.1.0'
+version '0.2.0'
 
 
 depends 'xfs'
