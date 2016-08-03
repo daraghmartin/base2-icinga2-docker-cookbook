db07cdd240529d1e4dccf73236584101fac4f092
daraghmartin <d.martin@base2services.com>
2016-08-04 04:50:41 +1000

    fine tuning

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,6 +1,8 @@
 default["base2"]["icinga2"]["pagerduty"]["api_key"] = "x"
-default["base2"]["icinga2"]["check_base2"] = true
+default["base2"]["icinga2"]["check_base2"] = false
 default["base2"]["icinga2"]["container"]["tag"] = 'v0.1.0'
+default["base2"]["icinga2"]["org"] = "some org - please change"
+
 default["base2"]["icinga2"]["container"]["binds"] = {
   '/data/opt/icinga2/extra' => '/opt/icinga2/extra',
   '/data/icinga2/mysql' => '/var/lib/mysql',
