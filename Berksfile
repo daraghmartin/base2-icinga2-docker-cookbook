bd72234bb80b1ffb3be7feb29fb62304e3725a9e
daragh martin <daraghmartin@daraghs-MacBook-Pro.local>
2016-09-16 10:57:05 +1000

    big change

--- Berksfile
+++
@@ -1,6 +0,0 @@
-source 'https://supermarket.chef.io'
-
-metadata
-cookbook 'xfs', '2.0.1'
-cookbook 'lvm', '2.1.0'
-cookbook 'docker', '2.7.1'
--- test/integration/default/serverspec/default_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-describe 'base2-icinga2-docker::default' do
-  # Serverspec examples can be found at
-  # http://serverspec.org/resource_types.html
-  it 'does something' do
-    skip 'Replace this with meaningful tests'
-  end
-end
